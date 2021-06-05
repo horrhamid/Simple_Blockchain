834740e9d22b1898dde33918a949a3bf271a0660
hamidreza sadeghi <xhamid0011@gmail.com>
2021-06-06 02:09:46 +0430

    blockchain system completed

--- blockchain.py
+++ blockchain.py
@@ -5,78 +5,69 @@ import os
 BlockChain = []
 BlockNumber = 1
 PrevHash = ''
-Hash = ''
 Nonce = 0
-chain = {
-    'Block number': '',
-    'Block hash': '',
-    'Prev hash': '',
-    'Nonce value': ''
-}
+
 Ledger_location = "Ledgers"
 Math_location = "Math_Problems"
 Ledger = ''
 problem = ''
 
 
-def sha256digest(fname):
-    return hashlib.sha256(open(fname, 'rb').read())
+def readfromjson(fname):
+    return json.load(open(fname,))
+
 
 addr = os.path.join("GenesisBlock", "GenesisBlock.json")
-Hash = sha256digest(addr)
-chain = {
+data_j = readfromjson(addr)
+data = json.dumps(data_j)
+sha = hashlib.sha256()
+sha.update(data.encode())
+Hash = sha.hexdigest()
+block = {
     'Block number': BlockNumber,
-    'Block hash': Hash.hexdigest(),
+    'Block hash': Hash,
     'Prev hash': PrevHash,
-    'Nonce value': str(Nonce)
+    'Nonce value': ''
 }
-print(Hash.hexdigest())
-BlockChain.append(chain)
+BlockChain.append(block)
+print(block)
 PrevHash = Hash
 BlockNumber += 1
 
 ##
 
 
-#reading ledger
-Ledger_location2 = "Ledger_Number" + str(BlockNumber) + ".json"
-l_file = os.path.join(Ledger_location, Ledger_location2)
-temp = sha256digest(l_file)
-print("Ledger" + temp.hexdigest())
-print (temp)
-Math_location2 = "Math_Problem_Number" + str(BlockNumber) + ".json"
-m_file = os.path.join(Math_location, Math_location2)
-file = open(m_file)
-math = json.load(file)
-number = math["mathProblem"]
-print(number)
-temp.update(PrevHash)
-print(temp.hexdigest())
-T = temp
+# reading ledger
+for BlockNumber in range(2, 12):
+    Ledger_location2 = "Ledger_Number" + str(BlockNumber) + ".json"
+    l_file = os.path.join(Ledger_location, Ledger_location2)
+    data_j = readfromjson(l_file)
+    data = json.dumps(data_j)
+    Math_location2 = "Math_Problem_Number" + str(BlockNumber) + ".json"
+    m_file = os.path.join(Math_location, Math_location2)
+    math_data = readfromjson(m_file)
+    Math_prob = math_data["mathProblem"]
+    # print(Math_prob)
+    while True:
+        temp = data + PrevHash + str(Nonce)
+        sha = hashlib.sha256()
+        sha.update(temp.encode())
+        Hash = sha.hexdigest()
+        # print("Nounce: " +str(Nonce))
 
-temp.update(str(Nonce).encode())
-print(temp.hexdigest())
-# tx = temp.hexdigest()
-
-Nonce += 1
-
-
-
-#
-# for BlockNumber in range(11):
-#     #reading ledger
-#     l_file = Ledger_location + str(BlockNumber) + ".json"
-#     temp = sha256digest(l_file)
-#     m_file = Math_location + str(BlockNumber) + ".json"
-#     file = open(m_file)
-#     math = json.load(file)
-#     number = math["mathProblem"]
-#     temp.update(PrevHash)
-#     T = temp
-#     while True:
-#         temp.update(str(Nonce).encode())
-#         tx = temp.hexdigest()
-#
-#         Nonce += 1
-#
+        if (Hash[-(len(str(Math_prob))):] == str(Math_prob)):
+            # problem solved
+            block = {
+                'Block number': BlockNumber,
+                'Block hash': Hash,
+                'Prev hash': PrevHash,
+                'Nonce value': Nonce
+            }
+            PrevHash = Hash
+            Nonce = 0
+            print(block)
+            BlockChain.append(block)
+            break
+        else:
+            Nonce += 1
 
